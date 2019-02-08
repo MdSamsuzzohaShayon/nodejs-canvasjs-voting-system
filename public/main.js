1c3ee444b3e8f26e31567310d4aa33a33db00fd2
Md Shayon <mdshayon0@gmail.com>
2019-02-09 03:20:54 +0600

    finished project

--- public/main.js
+++ public/main.js
@@ -27,68 +27,75 @@ form.addEventListener('submit', (e) => {
 
 // THIS TARGET GET REQUEST
 fetch('http://localhost:3000/poll')
-    .then(res=> res.json())
+    .then(res => res.json())
     .then(data => {
-        console.log(data);
-
-    });
-
-
-
-let dataPoints = [{
-        label: 'Windows',
-        y: 0
-    },
-    {
-        label: 'MacOS',
-        y: 0
-    },
-    {
-        label: 'Linux',
-        y: 0
-    },
-    {
-        label: 'Others',
-        y: 0
-    }
-];
+            // console.log(data);
+            const votes = data.votes;
+            const totalVotes = votes.length;
+            //COUNT VOTE POINTS
+            const voteCounts = votes.reduce((acc, vote) => (
+                (acc[vote.os] = (acc[vote.os] || 0) + parseInt(vote.points)), acc),
+                {}
+            );
+            // const voteCounts = votes.reduce(acc, vote) => ((acc[vote.os] = (acc[vote.os] || 0) + parseInt(vote.points)), acc), {});
 
 
-const chartContainer = document.querySelector('#chartContainer');
 
-if (chartContainer) {
-    const chart = new CanvasJS.Chart('chartContainer', {
-        animationEnabled: true,
-        theme: 'theme1',
-        title: {
-            text: 'OS Results'
-        },
-        data: [{
-            type: 'column',
-            dataPoints: dataPoints
-        }]
-    });
-    chart.render();
-
-    // https://dashboard.pusher.com/apps/709936/getting_started
-    // Enable pusher logging - don't include this in production
-    Pusher.logToConsole = true;
-
-    var pusher = new Pusher('b976189345ad44e8dfb4', {
-        cluster: 'ap2',
-        forceTLS: true
-    });
-
-    var channel = pusher.subscribe('os-poll');
-    channel.bind('os-vote', function (data) {
-        dataPoints = dataPoints.map(x => {
-            if (x.label == data.os) {
-                x.y += data.points;
-                return x;
-            } else {
-                return x;
+        let dataPoints = [{
+                label: 'Windows',
+                y: voteCounts.Windows
+            },
+            {
+                label: 'MacOS',
+                y: voteCounts.MacOS
+            },
+            {
+                label: 'Linux',
+                y: voteCounts.Linux
+            },
+            {
+                label: 'Others',
+                y: voteCounts.Others
             }
-        });
-        chart.render();
+        ];
+
+
+        const chartContainer = document.querySelector('#chartContainer');
+
+        if (chartContainer) {
+            const chart = new CanvasJS.Chart('chartContainer', {
+                animationEnabled: true,
+                theme: 'theme1',
+                title: {
+                    text: `Total Votes ${totalVotes}`
+                },
+                data: [{
+                    type: 'column',
+                    dataPoints: dataPoints
+                }]
+            });
+            chart.render();
+
+            // https://dashboard.pusher.com/apps/709936/getting_started
+            // Enable pusher logging - don't include this in production
+            Pusher.logToConsole = true;
+
+            var pusher = new Pusher('b976189345ad44e8dfb4', {
+                cluster: 'ap2',
+                forceTLS: true
+            });
+
+            var channel = pusher.subscribe('os-poll');
+            channel.bind('os-vote', function (data) {
+                dataPoints = dataPoints.map(x => {
+                    if (x.label == data.os) {
+                        x.y += data.points;
+                        return x;
+                    } else {
+                        return x;
+                    }
+                });
+                chart.render();
+            });
+        }
     });
-}
